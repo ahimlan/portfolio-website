4ac83b272fd8f76a51a0129b2149b72474281eb8
ahimlan <amandahimlan@yahoo.com>
2025-03-10 18:13:46 -0400

    Task 2.3 changes

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,2 +1,16 @@
 alert("Thanks for visiting my Portfolio Site!")
 console.log("Thanks for visiting my Portfolio Site!")
+
+
+var escapedText1 = "He said: \"Yes!\", that's for sure";
+console.log (Text1)
+
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
